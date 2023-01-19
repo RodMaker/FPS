342e9c51da8b5baf3f3cf4b15a731ca60418017b
RodMaker <83619630+RodMaker@users.noreply.github.com>
2023-01-19 16:30:31 +0000

    Episode 2.5

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
+    // Add or remove an InteractionEvent component to this gameobject
+    public bool useEvents;
     // Message displayed to player when looking at an interactable
+    [SerializeField]
     public string promptMessage;
+
+    public virtual string OnLook()
+    {
+        return promptMessage;
+    }
 
     // This function will be called from our player
     public void BaseInteract()
     {
+        if (useEvents)
+            GetComponent<InteractionEvent>().OnInteract.Invoke();
         Interact();
     }
 
